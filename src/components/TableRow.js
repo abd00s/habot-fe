013e91c60977473480eec50502463e45c2979a72
Abdullah Pharaon <abdullah.pharaon@gmail.com>
2019-04-08 16:46:18 -0400

    fetch and display all goals on index

---
+++ src/components/TableRow.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+
+class TableRow extends Component {
+  render() {
+    return (
+        <tr>
+          <td>
+            {this.props.obj.title}
+          </td>
+          <td>
+            {this.props.obj.frequency}
+          </td>
+          <td>
+            {this.props.obj.period}
+          </td>
+          <td>
+            <button>Edit</button>
+          </td>
+          <td>
+            <button>Delete</button>
+          </td>
+        </tr>
+    );
+  }
+}
+
+export default TableRow;
--- src/components/index.component.js
+++ src/components/index.component.js
@@ -1,11 +1,49 @@
 import React, { Component } from 'react';
+import axios from 'axios';
+import TableRow from './TableRow';
 
 export default class Index extends Component {
+
+    constructor(props) {
+      super(props);
+      this.state = {goals: []};
+    }
+
+    componentDidMount(){
+      axios.get('http://localhost:3001/goals')
+        .then(response => {
+          this.setState({ goals: response.data });
+          console.log(response.data);
+        })
+        .catch(function (error) {
+          console.log(error);
+        })
+    }
+
+    tabRow(){
+      return this.state.goals.map(function(object, i){
+          return <TableRow obj={object} key={i} />;
+      });
+    }
+
     render() {
         return (
             <div>
-                <p>Welcome to Index Component!!</p>
-            </div>
+                <p>Goals</p>
+              <table style={{ marginTop: 20 }}>
+              <thead>
+                <tr>
+                  <th>Title</th>
+                  <th>Frequency</th>
+                  <th>Period</th>
+                  <th>Action</th>
+                </tr>
+              </thead>
+              <tbody>
+                { this.tabRow() }
+              </tbody>
+            </table>
+          </div>
         )
     }
 }
